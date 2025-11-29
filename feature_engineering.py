7bdc43b239429c5a42ec2671bc013bf2dd980f86
chaudharyanjali581-svg <chaudharyanjali581@gmail.com>
2025-11-29 14:59:14 +0530

    fix column name

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,4 +1,6 @@
 def create_features(df):
-    df['total_guests'] = df['no_of_adults'] + df['no_of_children']
-    df['is_weekend'] = (df['arrival_date_day_of_month'] % 7 == 0).astype(int)
+    if "arrival_date_day_of_month" in df.columns:
+        df['is_weekend'] = (df['arrival_date'] % 7 == 0).astype(int)
+    else:
+        df['is_weekend']=0
     return df
